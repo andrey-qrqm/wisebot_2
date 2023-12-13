402802af427c9e70d6476554d9b04ca0738599a5
Andrey QRQM <andrey.madyarov@gmail.com>
2023-12-13 17:53:05 +0300

    Cleaning

--- responses.py
+++ responses.py
@@ -4,19 +4,9 @@ import random_event
 def handle_response(message, author) -> str:
     p_message = message.lower()
 
-    if p_message == 'hello':
-        return " Shut your mouth motherfucker!"
     if p_message == 'help':
         full = "Антон наконец меня заебал, \n/event роль - ивент на конкретную роль.\n/role - рандомная роль. \n/clear amount - удалить последние n сообщений, по дефолту удаляется 20\nимена - список кого может обосрать бoт"
         return full
-    if p_message == 'антон':
-        return 'негр и пидорас, но хороший человек'
-    if p_message == 'анур':
-        return 'резиновая уточка этого сервера'
-    if p_message == 'женя':
-        return 'багеты, негры, лягушки, фетиши севы - это все к ней'
-    if p_message == 'макс':
-        return 'любитель 40 отчимов'
     if p_message[:5] == 'event':
         if len(p_message)>5:
             arg_role = p_message[6:]
@@ -26,18 +16,8 @@ def handle_response(message, author) -> str:
             return 'WiseBot хочет чтобы '+ author + ' ' + random_event.event(arg_role)
         else:
             return 'Роль не указана, WiseBot хочет чтобы ' + author + ' ' + random_event.event(arg_role)
-    if p_message == 'андрей':
-        return 'ебал в рот гит и всех в мут нахуй'
-    if p_message == 'сева':
-        return 'хентай бойчик опять мисснул карой'
-    if p_message == 'аким':
-        return 'душнит так что хуй залупа муравей в ахуе'
-    if p_message == 'автор':
-        return get_nickname(author)
     if p_message == 'роль':
         return 'WiseBot хочет ' + get_nickname(author) + ' ' + random_event.random_role()
-    if p_message == 'имена':
-        return "антон, анур, женя, андрей, сева, макс, аким"
 
 def get_nickname(author):
     try:
